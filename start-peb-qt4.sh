58be774e884048c3cb531f55dac985546bc8991d
ddmitov <ddmitov@yahoo.com>
2013-12-29 03:31:54 +0200

    win32 loader script

--- start-peb-qt4.sh
+++ start-peb-qt4.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 # http://lemirep.wordpress.com/2013/06/01/deploying-qt-applications-on-linux-and-windows-3/
-export LD_LIBRARY_PATH=$(pwd)/qt5/lib
-export QT_QPA_PLATFORM_PLUGIN_PATH=$(pwd)/qt5/platforms
+export LD_LIBRARY_PATH=$(pwd)/qt4/i386/lib
 export QT_ACCESSIBILITY=0
-./peb-qt5
+./peb-qt4
---
+++ start-peb-qt5.cmd
@@ -0,0 +1,5 @@
+
+SET PATH=%cd%\qt5\win32\lib;%PATH%
+SET QT_QPA_PLATFORM_PLUGIN_PATH=%cd%\qt5\win32\platforms
+peb.exe
+
